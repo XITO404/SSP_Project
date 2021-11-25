4bfa1e909d4aa3228c4f6c84c24e1c243385d0f9
gowneunju <kkkoj4284@naver.com>
2021-11-25 17:04:52 +0900

    feat : calendar.html - reaction(emoji), calendar.css, reaction.js, reaction.css

---
+++ static/js/reaction.js
@@ -0,0 +1,65 @@
+function count_1(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_1');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count_2(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_2');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count_3(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_3');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count_4(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_4');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count_5(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_5');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count_6(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result_6');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  // 더하기
+    number = parseInt(number) + 1;
+  // 결과 출력
+  resultElement.innerText = number;
+}
