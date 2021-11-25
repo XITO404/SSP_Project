8b7eb3c32d6320212340d35a1b0061933744cdff
Prcnsi <ji.dozin@gmail.com>
2021-11-25 13:31:39 +0900

    Sort the file structure and link the file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+# app.py는 서버를 돌리는 파일
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 템플릿 랜더링은 아래 구조로 사용
+# @app.route('경로')
+# def 파일명():
+#   return render_template('파일명.확장자')
 @app.route('/')
-def basic():
-    return render_template("basic.html")
+def mainpage():
+    return render_template("mainpage.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
