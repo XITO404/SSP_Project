13f06ff2673e864c9491620d927a3f95c770ab7e
Prcnsi <ji.dozin@gmail.com>
2021-11-26 17:27:40 +0900

    오타 수정

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def mainpage():
     return render_template("mainpage.html")
 
+#@app.route()
+def signup():
+    return render_template("signup.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
